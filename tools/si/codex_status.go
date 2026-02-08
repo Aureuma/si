b3acdfba3c52f542cbfb0149e1321b9fff907bd1
SHi-ON <shawn@azdam.com>
2026-02-08 17:07:14 +0000

    chore(release): cut v0.47.0
    - Align changelog notes for installer/vault across v0.45.0-v0.47.0\n- Bump codex app-server clientInfo.version to 0.47.0

--- tools/si/codex_status.go
+++ tools/si/codex_status.go
@@ -478,7 +478,7 @@ func buildAppServerInput() []byte {
 			Params: map[string]interface{}{
 				"clientInfo": map[string]string{
 					"name":    "si",
-					"version": "1.2.0",
+					"version": "0.47.0",
 				},
 			},
 		},
